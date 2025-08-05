d77844505d6ab5d64d288184cf862c1ed49b2c81
David Reis <100007625+uncoder21@users.noreply.github.com>
2025-08-05 13:14:11 -0300

    Fix lexer implementation and update test usage

--- src/UIL/Binding/IBinder.cs
+++ src/UIL/Binding/IBinder.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UIL.Diagnostics;
 using UIL.Symbols;
 using UIL.Syntax;
@@ -8,5 +9,6 @@ public interface IBinder
 {
     DiagnosticBag Diagnostics { get; }
     void BindCompilationUnit(CompilationUnitSyntax syntax);
+    IReadOnlyDictionary<string, TypeSymbol> Types { get; }
     BoundBlockStatement BindMethod(MethodDeclarationSyntax syntax, out MethodSymbol symbol);
 }
